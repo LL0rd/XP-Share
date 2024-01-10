711face2bb7b4f04aea5bc162bffd041d6205824
Dimitri Roschkowski <info@d-r.dev>
2024-01-10 16:06:01 +0200

    - Changed App Name to XP-Share

--- webapp/constants/metadata.js
+++ webapp/constants/metadata.js
@@ -1,9 +1,9 @@
 // this file is duplicated in `backend/src/config/metadata.js` and `webapp/constants/metadata.js` and replaced on rebranding
 export default {
-  APPLICATION_NAME: 'ocelot.social',
-  APPLICATION_SHORT_NAME: 'ocelot.social',
-  APPLICATION_DESCRIPTION: 'ocelot.social Community Network',
-  COOKIE_NAME: 'ocelot-social-token',
-  ORGANIZATION_NAME: 'ocelot.social Community',
+  APPLICATION_NAME: 'XP-Share',
+  APPLICATION_SHORT_NAME: 'XP-Share',
+  APPLICATION_DESCRIPTION: 'XP-Share  Network',
+  COOKIE_NAME: 'xp-token',
+  ORGANIZATION_NAME: 'XP-Share Community',
   ORGANIZATION_JURISDICTION: 'City of Angels',
 }
